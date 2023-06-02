325874b5b348ea476319fee08c078e22a3a9c440
Vladimir Leveryev <volodya.leveryev@gmail.com>
2023-06-02 23:24:56 +0900

    Добавлен favicon

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from base import views as base_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', base_views.index_page),
 ]
